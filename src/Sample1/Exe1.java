af04e26cd19dd89279c5fbdff90f743f56765cfa
“ajaykumar15893” <ajayswatantra@gmail.com>
2024-11-30 13:02:39 +0530

    EXe added

---
+++ src/Sample1/Exe1.java
@@ -0,0 +1,8 @@
+package Sample1;
+
+public class Exe1 
+{
+public static void main(String[] args) {
+	System.out.println("hii myself ajay");
+}
+}
---
+++ src/Sample1/Exe2.java
@@ -0,0 +1,8 @@
+package Sample1;
+
+public class Exe2
+{
+public static void main(String[] args) {
+	System.out.println("runnig exe2 ");
+}
+}
